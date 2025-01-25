a90cd60e390e3e68c2e171f56d13f9ca64b9490e
Dragos Serban <94605831+DragosSerban@users.noreply.github.com>
2025-01-25 12:50:00 +0200

    Delete tema-2-DragosSerban-master directory

--- tema-2-DragosSerban-master/src/main/java/org/example/CererePensionar.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class CererePensionar extends Cerere {
-    // constructor pt cererile utilizatorilor de tip Pensionar
-    CererePensionar(String nume, String cerere, String data, int prioritate) {
-        super(nume, cerere, data, prioritate);
-    }
-}
--- tema-2-DragosSerban-master/src/main/java/org/example/CererePersoana.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class CererePersoana extends Cerere {
-    // constructor pt cererile utilizatorilor de tip Persoana
-    CererePersoana(String nume, String cerere, String data, int prioritate) {
-        super(nume, cerere, data, prioritate);
-    }
-}
